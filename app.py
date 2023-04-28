427c4f9d671cd5865bb8c57c9081b4453c968be8
ShaniceWilliams <shanice.williams@live.co.uk>
2023-04-28 14:31:23 +0100

    Update to receive user input to change parameters in the app.

--- app.py
+++ app.py
@@ -9,11 +9,16 @@ Shown are the stock closing price and volume of Google!
 
 """)
 
-tickerSymbol = 'GOOGL'
+tickerSymbol = st.text_input('Enter the ticker of the company you would like to see the data for: ')
+period = st.selectbox('Period:', ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'])
+start = st.date_input('Start date:')
+end = st.date_input('End date:')
+interval = st.selectbox('Interval:', ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'])
 
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period=period, start=start, end=end, interval=interval)
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
+
